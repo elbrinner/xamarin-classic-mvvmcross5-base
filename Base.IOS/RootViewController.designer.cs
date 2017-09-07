58b6f94d72e8a3db8fdd6944044ac97e711fbed6
ELBRINNERDA8166\elbrinner <elbrinner@hotmail.com>
2017-09-07 08:47:54 -0300

    Proyecto con la configuración base para IOS, Android y UWP con mvvmcross 5.1

---
+++ Base.IOS/RootViewController.designer.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Base.IOS
+{
+	[Register ("RootViewController")]
+	partial class RootViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
+
---
+++ Base.UWP/Setup.cs
@@ -0,0 +1,30 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform.Plugins;
+using MvvmCross.Uwp.Platform;
+using Windows.UI.Xaml.Controls;
+
+namespace Base.UWP
+{
+    public class Setup : MvxWindowsSetup
+    {
+        public Setup(Frame rootFrame) : base(rootFrame)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Core.App();
+        }
+
+        protected override IMvxPluginManager CreatePluginManager()
+        {
+            return base.CreatePluginManager();
+        }
+
+        protected override void InitializeFirstChance()
+        {
+
+        }
+    }
+}
+
